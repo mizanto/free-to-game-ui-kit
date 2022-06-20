77a1c5830caa0a8bb781b24eae20d02b6395a2be
Sergey Bendak <sleepoke@gmail.com>
2022-06-20 12:14:05 +0300

    add an empty project

---
+++ free-to-game-ui-kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  free-to-game-ui-kit
+//
+//  Created by Sergey Bendak on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
